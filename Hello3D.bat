e6e030d8664f0c2a5d8f9cc3a040d59b3e7d78b0
jelo.wang@gmail.com <jelo.wang@gmail.com@84643461-4d9c-033f-1849-ac4810d19ea7>
2013-01-31 06:21:45 +0000

    commit i51 files

--- Hello3D.bat
+++ Hello3D.bat
@@ -1,21 +1,2 @@
-copy ..\..\i51TOOL\tcc.exe *.exe
-copy ..\..\i51TOOL\armlink.exe *.exe
-copy ..\..\i51TOOL\armcc.exe *.exe
-copy ..\..\i51TOOL\armcpp.exe *.exe
-copy ..\..\i51TOOL\armar.exe *.exe
-copy ..\..\i51TOOL\i51Compiler.exe *.exe
-copy ..\..\i51TOOL\PackageMaker.exe *.exe
-copy ..\..\i51TOOL\Semo.exe *.exe
-Semo Hello3D.se
-i51Compiler Hello3D.se
-PackageMaker.exe Hello3D -re -kit -i51
-del *.exe
-del *.o
-del *.axf
-
-if not exist ..\..\i51EMU\WIN32FS\DRIVE_E\i51\i51KIT\Hello3D md ..\..\i51EMU\WIN32FS\DRIVE_E\i51\i51KIT\Hello3D
-if exist *.i51 copy *.i51 ..\..\i51EMU\WIN32FS\DRIVE_E\i51\i51KIT\Hello3D\*.i51
-if exist *.kit copy *.kit ..\..\i51EMU\WIN32FS\DRIVE_E\i51\i51KIT\Hello3D\*.kit
-if exist *.re copy *.re ..\..\i51EMU\WIN32FS\DRIVE_E\i51\i51KIT\Hello3D\*.re
-if exist .\Debug\*.dll copy .\Debug\*.dll ..\..\i51EMU\WIN32FS\DRIVE_E\i51\i51KIT\Hello3D\*.dll
+if exist .\Debug\*.dll copy .\Debug\*.dll ..\..\i51EMU\WIN32FS\DRIVE_E\i51\i51KIT\i51SapBooter\i51SapBooter.dll
 
